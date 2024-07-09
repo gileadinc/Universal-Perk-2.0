abf3a19f0838a55fce25acdab4ea826f61ccf7f0
tinsaye-up <tsimeneh@universalperks.com>
2024-07-09 23:18:10 +0300

    fix: footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,14 @@
+"use client";
+import React, { useState } from "react";
 import { Container, Text, Group, Anchor } from "@mantine/core";
+
+const links = [
+  { link: "/#hero", label: "Home" },
+  { link: "/#about", label: "About Us" },
+  { link: "/#services", label: "Services" },
+  { link: "/#projects", label: "Projects" },
+  { link: "/contact", label: "Contact Us" },
+];
 
 export default function Footer() {
   return (
@@ -16,12 +26,22 @@ export default function Footer() {
           © 2024 Universal Perk. All rights reserved.
         </Text>
       </Group>
-      <Group align="center">
-        <Anchor href="/projects" style={{ marginRight: "1rem" }}>
-          Projects
-        </Anchor>
-        <Anchor href="/contact">Contact Us</Anchor>
-      </Group>
+      <header className="sticky top-0 px-10 text-black">
+        <Container size="md" className="flex items-center justify-between py-4">
+          <Text className="text-gray-900 dark:text-white">Universal Perk</Text>
+          <nav className="hidden space-x-4 md:flex">
+            {links.map((link) => (
+              <a
+                key={link.label}
+                href={link.link}
+                className={`cursor-pointer text-gray-700 dark:text-black hover:text-gray-900 dark:hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium `}
+              >
+                {link.label}
+              </a>
+            ))}
+          </nav>
+        </Container>
+      </header>
     </Container>
   );
 }
