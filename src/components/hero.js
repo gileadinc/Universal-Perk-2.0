5670a4437afe7d8d53f788fa40e14b3c0d463436
green <nzebene@universalperk.com>
2024-07-09 15:57:17 -0400

    fix/ added smooth animation on section navigation

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,7 +4,13 @@ import React from "react";
 import Typewriter from "./TypeWritter";
 
 
+
+
 const Hero = () => {
+
+    const scrollToSection = (id) => {
+        document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+      };
   
     return (
         <div className="bg-white" id="home">
@@ -61,6 +67,10 @@ const Hero = () => {
                                         <a
                                             href="#contact"
                                             className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                            onClick={(e) => {
+                                                e.preventDefault();
+                                                scrollToSection("contact")
+                                            }}
                                         >
                                             Contact us
                                         </a>
