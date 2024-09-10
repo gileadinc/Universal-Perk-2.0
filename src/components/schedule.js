088e7175919c14d5e29bf1b80c547aa0cc644aff
green <green@green.green>
2024-09-10 11:38:13 -0400

    fix/fixed all issues given on the document

--- src/components/schedule.js
+++ src/components/schedule.js
@@ -1,43 +1,86 @@
 const Schedule = () => {
-    return (
-        <div className="sm:px-24" id="schedule">
-            <div className="bg-indigo-700 py-16 sm:py-24 lg:py-32 rounded-sm">
-                <div className="mx-auto grid max-w-7xl grid-cols-1 gap-10 px-6 lg:grid-cols-12 lg:gap-8 lg:px-8">
-                    <div className="max-w-xl text-3xl font-bold tracking-tight text-white sm:text-4xl lg:col-span-7">
-                        <h2 className="inline sm:block lg:inline xl:block">Schedule a free
-                            30 mins Consultation</h2>{' '}
-                        <p className="mt-6 text-lg leading-8 text-gray-300">
-                            Explore your options with a complimentary 30-minute consultation tailored to your needs. Discover how our expertise can help you achieve your goals
-                        </p>
-
-                    </div>
-                    <form className="w-full max-w-md lg:col-span-5 lg:pt-2">
-                        <div className="flex gap-x-4">
-                            <label htmlFor="email-address" className="sr-only">
-                                Email address
-                            </label>
-                            <input
-                                id="email-address"
-                                name="email"
-                                type="email"
-                                required
-                                placeholder="Enter your email"
-                                autoComplete="email"
-                                className="min-w-0 flex-auto rounded-md border-0 bg-white/10 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 placeholder:text-white/75 focus:ring-2 focus:ring-inset focus:ring-white sm:text-sm sm:leading-6"
-                            />
-                            <button
-                                type="submit"
-                                className="flex-none rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-indigo-600 shadow-sm hover:bg-indigo-50 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
-                            >
-                                Schedule
-                            </button>
-                        </div>
-
-                    </form>
-                </div>
-            </div>
+  const images = [
+    {
+      path: "/react.svg",
+      alt: "react",
+    },
+    {
+      path: "/javascript.svg",
+      alt: "Javascript",
+    },
+    {
+      path: "/angular.svg",
+      alt: "Angular",
+    },
+    {
+      path: "/aws.svg",
+      alt: "AWS",
+    },
+    {
+      path: "/d3.svg",
+      alt: "D3",
+    },
+    {
+      path: "/flutter.svg",
+      alt: "Flutter",
+    },
+    {
+      path: "/gcp.svg",
+      alt: "Google cloud",
+    },
+    {
+      path: "/css.svg",
+      alt: "Css",
+    },
+    {
+      path: "/html.svg",
+      alt: "HTML",
+    },
+    {
+      path: "/azure.svg",
+      alt: "Azure",
+    },
+    {
+      path: "/node-js.svg",
+      alt: "Node js",
+    },
+    {
+      path: "/vue.svg",
+      alt: "Vue js",
+    },
+    {
+      path: "/java.svg",
+      alt: "Java"
+    }
+  ];
+  return (
+    <div className="flex flex-col items-center gap-y-10">
+      <div className="flex flex-col items-center">
+        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          Technologies
+        </h2>
+        <p className="mt-2 text-lg leading-8 text-gray-600">
+          Technologies we use to develop systems
+        </p>
+      </div>
+      <div className="flex items-center justify-center">
+        <div className="overflow-hidden w-[90%]">
+          <div className="flex items-center justify-end space-x-[100px] animate-loop-scroll">
+            {images.map((image, index) => (
+              <div key={index} className="max-w-30">
+                <img
+                  loading="lazy"
+                  src={image.path}
+                  className="w-full h-auto object-fit"
+                  alt={image.alt}
+                />
+              </div>
+            ))}
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Schedule
+export default Schedule;
