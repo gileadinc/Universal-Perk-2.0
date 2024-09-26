eec656dab3f44e607cbf34a0f8f2c70271a99d94
green <nzebene@universalperk.com>
2024-09-26 08:08:49 -0400

    fix/changed headers on details

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -66,88 +66,9 @@ export default function About() {
   return (
     <>
       <div className="bg-gray-900">
-        <div className="relative isolate pt-14">
-          <header className="absolute inset-x-0 top-0 z-50">
-            <nav
-              aria-label="Global"
-              className="flex items-center justify-between p-6 lg:px-8"
-            >
-              <div className="flex lg:flex-1">
-                <a href="#" className="-m-1.5 p-1.5">
-                  <span className="sr-only">Universal Perk</span>
-                  <img
-                    alt=""
-                    src="/logo.svg"
-                    className="h-20 w-auto"
-                  />
-                </a>
-              </div>
-              <div className="flex lg:hidden">
-                <button
-                  type="button"
-                  onClick={() => setMobileMenuOpen(true)}
-                  className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-                >
-                  <span className="sr-only">Open main menu</span>
-                  <Menu aria-hidden="true" className="h-6 w-6" />
-                </button>
-              </div>
-              <div className="hidden lg:flex justify-center w-[90%] lg:gap-x-12">
-                {navigation.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    className={`text-sm font-semibold leading-6 text-white`}
-                  >
-                    {item.name}
-                  </a>
-                ))}
-              </div>
-            </nav>
-            <Dialog
-              open={mobileMenuOpen}
-              onClose={setMobileMenuOpen}
-              className="lg:hidden"
-            >
-              <div className="fixed inset-0 z-50" />
-              <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-                <div className="flex items-center justify-between">
-                  <a href="#" className="-m-1.5 p-1.5">
-                    <span className="sr-only">Universal Perk</span>
-                    <img
-                      alt=""
-                      src="/logo.svg"
-                      className="h-8 w-auto"
-                    />
-                  </a>
-                  <button
-                    type="button"
-                    onClick={() => setMobileMenuOpen(false)}
-                    className="-m-2.5 rounded-md p-2.5 text-gray-700"
-                  >
-                    <span className="sr-only">Close menu</span>
-                    <X aria-hidden="true" className="h-6 w-6" />
-                  </button>
-                </div>
-                <div className="mt-6 flow-root">
-                  <div className="-my-6 divide-y divide-gray-500/10">
-                    <div className="space-y-2 py-6">
-                      {navigation.map((item) => (
-                        <a
-                          key={item.name}
-                          href={item.href}
-                          className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                        >
-                          {item.name}
-                        </a>
-                      ))}
-                    </div>
-                  </div>
-                </div>
-              </DialogPanel>
-            </Dialog>
-          </header>
-          <div className="py-24 sm:py-32 lg:pb-40">
+        <Header />
+        <div className="relative isolate pt-14 mt-20">
+          <div className="py-24 sm:py-32  lg:pb-40">
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
               <div className="mx-auto max-w-3xl text-center">
                 <h1 className="text-4xl font-bold text-[#4F46E5] tracking-tight text-white sm:text-6xl">
@@ -179,11 +100,11 @@ export default function About() {
         </div>
         <div className="w-full flex justify-start ">
           <p className="text-[#6B7280] text-base sm:text-[24px] font-[400]">
-             is to empower businesses by delivering cutting-edge
-            technology and tailored software solutions that help them thrive in
-            today’s dynamic market. With a client-focused approach, we
-            specialize in enhancing operational efficiency, improving business
-            processes, and providing secure and scalable IT infrastructure.
+            is to empower businesses by delivering cutting-edge technology and
+            tailored software solutions that help them thrive in today’s dynamic
+            market. With a client-focused approach, we specialize in enhancing
+            operational efficiency, improving business processes, and providing
+            secure and scalable IT infrastructure.
           </p>
         </div>
       </div>
