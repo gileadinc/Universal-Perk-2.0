548ddf8cfd18e79dd861f666a523c8608ccda771
green <nzebene@universal.com>
2024-09-26 07:37:43 -0400

    fix/changed landing page for das case study

--- src/components/hero_section.js
+++ src/components/hero_section.js
@@ -18,7 +18,7 @@ export default function Hero() {
         </div>
         <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
           <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
-            <div className="-m-2 rounded-xl p-2 lg:-m-4 lg:p-4 max-w-[70vh] max">
+            <div className="-m-2 rounded-xl p-2 lg:-m-4 lg:p-4 max-w-[70vh]">
               <img
                 alt="App screenshot"
                 src="/phone.png"
